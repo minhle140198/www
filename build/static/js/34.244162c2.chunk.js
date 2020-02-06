186b58d2845d29a46aec20eb5909ebc69000884c
minh le 1998 <minh.le@kyanon.digital>
2020-02-06 15:51:39 +0700

    build update

---
+++ build/static/js/34.244162c2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfiretable=this.webpackJsonpfiretable||[]).push([[34],{771:function(e,t,n){"use strict";n.r(t);var a=n(62),r=n(63),i=n(73),c=n(71),u=n(74),b=n(0),o=n.n(b),s=n(564),p=(n(568),function(e){function t(){return Object(a.a)(this,t),Object(i.a)(this,Object(c.a)(t).apply(this,arguments))}return Object(u.a)(t,e),Object(r.a)(t,[{key:"render",value:function(){return o.a.createElement(s.GEditor,{id:"editor",webpage:!0})}}]),t}(b.Component));t.default=p}}]);
+//# sourceMappingURL=34.244162c2.chunk.js.map
